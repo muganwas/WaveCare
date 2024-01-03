9d5a8607f4c2fddb2513ba200bd8b3b143813e9e
Muganwa <stevenmuganwa@live.com>
2024-01-03 10:04:17 +0300

    nav

--- public/scripts/main.js
+++ public/scripts/main.js
@@ -9,4 +9,31 @@
         else
             links.style.display = 'none';
     });
+    /** Nav */
+    const prodLink = doc.getElementById('products');
+    const aboutLink = doc.getElementById('about');
+    const contactLink = doc.getElementById('contact');
+    const navLinks = doc.getElementsByClassName('nav-link');
+    const loc = win.location.pathname;
+
+    if (loc.includes('products')) {
+        Array.from(navLinks).forEach(l => {
+            l.classList.remove('active');
+        });
+        prodLink.classList.add('active');
+    }
+    if (loc.includes('About')) {
+        Array.from(navLinks).forEach(l => {
+            l.classList.remove('active');
+        });
+        aboutLink.classList.add('active');
+    }
+    if (loc.includes('Contact')) {
+        Array.from(navLinks).forEach(l => {
+            l.classList.remove('active');
+        });
+        contactLink.classList.add('active');
+    }
+
+
 })(window, document);
